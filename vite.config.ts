6870521d2c87f4e0c9cdfd5e9030ef39f9803109
vgfang <vgfang@yandex.com>
2026-04-19 01:25:46 -0700

    fix project name for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/invoice-gen/",
+  base: "/construction-estimate-gen/",
   plugins: [react(), tailwindcss()],
 });
